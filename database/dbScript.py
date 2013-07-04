755147ae6ce6e1c16d7675883a5837b79df2ae10
LuckyLeon <newvava@gmail.com>
2013-07-03 22:28:56 -0700

    bach add images from spreadsheets

--- database/dbScript.py
+++ database/dbScript.py
@@ -1,19 +1,29 @@
 from pymongo import Connection
 import pymongo
 import datetime
+from boto.s3.connection import S3Connection
+import Image
+import urllib
+import cStringIO
+import csv
+
+#S3 storage
+myKey = "AKIAIXVQENDGQDLR33JA"
+mySecretKey = "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"
+myBucket = 'zwamy'
+
+#mongolab
 server = 'ds031948.mongolab.com'
 port = 31948
 db_name = 'zwamy'
 username = 'leonzwamy'
 password = 'zw12artistic'
-
-
 classes = ["user","image","comment","viewlist","play","client"]
-
+thumbnail_size = [960,500]
 
 
 def initIndexes(db):
-    #all indexes started from 0, so the none index is -1
+    #all indexes start from 0, so the none index is -1
     db.index.remove()
     indexObj = {}
     for cl in classes:
@@ -42,18 +52,16 @@ def createUser(db,usrObj):
         addObj[mustHave]= usrObj[mustHave]
         
     currIndex = db.index.find_one()['user']
-    addObj["id"] = currIndex+1
-    addObj['images'] = []
-    addObj['viewlists']=[]
-    addObj['friends']=[]
-    addObj['adds']=[]
-    addObj['requests']=[]
-    addObj['owned_clients']=[]
-    addObj['playing_clients'] = []
-    addObj['playable_clients'] = []
-    addObj['plays'] = []
+    usrObj["id"] = currIndex+1
     
-    db.users.insert(addObj)
+    emptyArrays = ["images","viewlists","friends","adds","requests","owned_clients",
+                    "playable_clients","playable_clients","plays"]
+    
+    for emptyArray in emptyArrays:
+        usrObj[emptyArray]=[]
+ 
+    
+    db.users.insert(usrObj)
     db.index.update({},{"$set":{"user":currIndex+1}})
     
     return True
@@ -120,11 +128,12 @@ def deleteUser(db, email):
 
 
 def currUTC():
+    # return a current utc datetime string
     #'2013-07-02 22:59:46'
     currTime = datetime.datetime.utcnow()
     return str(currTime).split('.')[0]   
 
-def createImage(db, imgObj):
+def createImage(db, bucket, imgObj):
     mustHaves = ["creator","url"]
     
     for mustHave in mustHaves:
@@ -139,16 +148,39 @@ def createImage(db, imgObj):
         print "user"+str(userID)+" doesn't exist"
         return False
     
-    userObj = userSet[0]
-    userObj["images"].append(currIndex+1)
-    db.users.update({"id":userID},{"$set":{"images":userObj["images"]}})
-    currIndex = db.index.find_one()['image']
+    
+    currIndex = db.index.find_one()['image']    
     imgObj["id"]=currIndex+1
     imgObj["datetime_created"]=currUTC()
-    db.images.insert(imgObj)
-    db.index.update({},{"$set":{"image":currIndex+1}})
+    imgObj["thumbnail"]='http://s3.amazonaws.com/zwamy/thumbnails/th'+str(currIndex+1)+'.jpg'
+    print imgObj["thumbnail"]
     
+    if makeThumbnail(bucket,imgObj["id"],imgObj["url"]):
+       userObj = userSet[0]
+       userObj["images"].append(currIndex+1)
+       db.users.update({"id":userID},{"$set":{"images":userObj["images"]}}) 
+       db.images.insert(imgObj)
+       db.index.update({},{"$set":{"image":currIndex+1}})
+    
+def makeThumbnail(bucket,imageID, url):
+    if url == "":
+        print "url cannot be empty!"
+        return False
+    text = urllib.urlopen(url = url).read()
+    if text[0]=='5':
+        print "image url error!"
+        return False
+    imData = cStringIO.StringIO(text)
+    img = Image.open(imData)
+    img.thumbnail(thumbnail_size)
+    image_name = 'thumbnails/th'+str(imageID)+".jpg"
+    key = bucket.new_key(image_name)
 
+    output = cStringIO.StringIO()
+    img.save(output,"JPEG")
+    key.set_contents_from_string(output.getvalue())
+    bucket.set_acl("public-read",image_name)
+    return True
 
 def deleteImage(db, imageID):
     print "to be done"
@@ -168,7 +200,7 @@ def addImageToViewlist(db, imageID, viewlistID):
     imgObj = imageSet[0]
     viewlistObj = viewSet[0]
     if imageID in viewlistObj["images"]:
-        print imageID+" is already in List"+viewlistID
+        print "image"+str(imageID)+" is already in viewlist"+str(viewlistID)
         return False
     
     imgObj["viewlists"].append(viewlistID)
@@ -182,7 +214,7 @@ def removeImageFromViewlist(db, imageID, viewlistID):
     print "to be done"
     
 def createViewlist(db, viewlistObj):
-    # don't add images yet
+    # haven't added images yet
     mustHaves = ["creator"]
     for mustHave in mustHaves:
         if not mustHave in viewlistObj:
@@ -262,8 +294,7 @@ def createPlay(db, playObj):
     #client
     #user
     #viewlist
-    #curr_index
-    
+    #curr_index   
     mustHaves = ["client","user","viewlist","curr_index"]
     for mustHave in mustHaves:
         if not mustHave in playObj:
@@ -298,24 +329,73 @@ def createPlay(db, playObj):
     currIndex = db.index.find_one()['play']
     playObj["id"]=currIndex+1
     playObj["datetime_created"]=currUTC()
-    db.plays.insert(playObj)
-    db.index.update({},{"$set":{"play":currIndex+1}})
+    playObj["last_update_time"]=playObj["datetime_created"]
+        
     #append the new play to the plays of the user
     userObj["plays"].append(currIndex+1)
     db.users.update({"id":userID},{"$set":{"plays":userObj["plays"]}})
+    
+    
+    # inactivate the play on the client
+    prevPlayID = clientObj["curr_play"]
+    if prevPlayID != -1:
+       db.plays.update({"id":prevPlayID},{"$set":{"active":0}}) 
+    
     #the curr_play of the client is set as the new play
     db.clients.update({"id":clientID},{"$set":{"curr_play":currIndex+1}})
-    return True
     
-def updatePlay(db, playID,curr_index):
-    # if viewlist is changed, we say a new play is started, instead we should call the function createPlay\
-    # updatePlay is only for swiping
-    playSet = db.plays.find({"playID"})
-    if playSet.count()==0:
-        print "play"+playID+" does't exist"
-        return False
-    db.plays.update({"id":playID},{"$set":{"curr_index":curr_index,"last_update_time":currUTC()}})
+    playObj["active"] = 1   #since the client is updated to have the current play, the play is active
+    db.plays.insert(playObj)
+    db.index.update({},{"$set":{"play":currIndex+1}})
+    
     return True
+
+def rerievePlay(db,userID,clientID):
+    #retrieve the play if it exists, and synchronize the client
+    playSet = db.plays.find({"user":userID,"client":clientID})
+    if playSet.count()==0:
+        print "user"+str(userID)+" has never played on client"+str(clientID)+" please create a new play!"
+        return None
+         
+    #update the client status to switch to this play
+    db.clients.update({"id":clientID},{"$set":{"curr_play":playID}})
+    
+    # the play is now active
+    playObj = playSet[0]
+    playObj["active"]=1
+    playID = playObj["id"]
+    db.plays.update({"id":playID},{"$set":{"active":1}})
+    return playObj
+
+
+
+   
+def updatePlay(db, playID, viewlistID,curr_index):
+    # result index
+    # 0: play or viewlist doesn't exist
+    # 1: play inactive
+    # 2: successfully updated
+    results = {"not_exist":0,"inactive":1,"success":2}
+    
+    playSet = db.plays.find({"id":playID})
+    if playSet.count()==0:
+        print "play"+str(playID)+" doesn't exist"
+        return results["not_exist"]
+    
+    viewlistSet = db.viewlists.find({"id":viewlistID})
+    if viewlistSet.count()==0:
+        print "viewlist"+str(viewlistID)+" doesn't exist"
+        return results["not_exist"]
+    
+    #update the play
+    db.plays.update({"id":playID},{"$set":{"viewlist":viewlistID,"curr_index":curr_index,"last_update_time":currUTC()}})
+    
+    playObj = playSet[0]
+    if playObj["active"]==0:
+        print "warning, your play on client"+str(playObj["client"])+" is inactive, to activate it, please retrieve the play"
+        return results["inactive"]
+    
+    return results["success"]
 
 def createClient(db, account, token, userID, nickname):
     #account must be unqiue
@@ -338,8 +418,7 @@ def createClient(db, account, token, userID, nickname):
 
 def deleteClient(db,clientId):
     print "to be done"
-    
-    
+        
       
 def deleteViewlist(db, viewlistID):
     print "to be done"
@@ -348,33 +427,73 @@ def deleteViewlist(db, viewlistID):
 def editViewlist(db, newViewlist):
     print "to be done"
          
-     
+
+def deletePlay(db, playID):
+    print "to be done"    
+    
+
+
+
+def test(db,bucket):
+    initIndexes(db)
+    db.users.remove()
+    db.images.remove()
+    createUser(db,{"name":"LuckyLeon", "email":"leon@zwamy.com","first_name":"Leon","last_name":"Zhu"})
+    createUser(db,{"email":"nancy@zwamy.com","first_name":"Nancy","last_name":"Laube"})  
+    
+    addImages(db, bucket, "img.csv",1)                     
+    
+    #createUser(db,{"name":"LeonZw", "email":"newvava@gmail.com",
+    #            "first_name":"Lew","last_name":"Zhu"})
+    #createUser(db,{"name":"Ruirui", "email":"zhangruiddn@gmail.com",
+    #            "first_name":"Rui","last_name":"Zhang"})
+    #addFriend(db, "leon@zwamy.com","zhangruiddn@gmail.com")
+    #addFriend(db, "zhangruiddn@gmail.com","leon@zwamy.com")
+    
+    #addFriend(db, "leon@zwamy.com","newvava@gmail.com")
+    #addFriend(db, "newvava@gmail.com","leon@zwamy.com")
+
+    #addFriend(db, "zhangruiddn@gmail.com","newvava@gmail.com")
+    #addFriend(db, "newvava@gmail.com","zhangruiddn@gmail.com")
+    #deleteUser(db, "zhangruiddn@gmail.com")
+
+
+def addImages(db, bucket, fileName, userID):
+    file = open(fileName,'rU')
+    reader = csv.reader(file)
+    attrs = ["Title","Year","Artist","Permission","url","Medium","Dimensions","Location"]
+            
+    
+    index = 0
+    for row in reader:
+        if index == 0:
+           index += 1
+           continue 
+        colIndex = 0
+        imgObj = {}
+        for content in row:
+           imgObj[attrs[colIndex]]=content
+           colIndex += 1
+        imgObj['creator'] = 1
+        print imgObj
+        createImage(db,bucket,
+                imgObj)
+        index+=1
+        
+    file.close()
+    
+
+
 
 def main():
     conn = Connection(server, port)
     db = conn[db_name]
     db.authenticate(username, password)
     
+    connS3 = S3Connection(myKey, mySecretKey)
+    bucket = connS3.get_bucket(myBucket)
     
-    initIndexes(db)
-    db.users.remove()
-    
-    
-    createUser(db,{"name":"LuckyLeon", "email":"leon@zwamy.com","first_name":"Leon","last_name":"Zhu"})
-    createUser(db,{"name":"LeonZw", "email":"newvava@gmail.com",
-                "first_name":"Lew","last_name":"Zhu"})
-    createUser(db,{"name":"Ruirui", "email":"zhangruiddn@gmail.com",
-                "first_name":"Rui","last_name":"Zhang"})
-    addFriend(db, "leon@zwamy.com","zhangruiddn@gmail.com")
-    addFriend(db, "zhangruiddn@gmail.com","leon@zwamy.com")
-    
-    addFriend(db, "leon@zwamy.com","newvava@gmail.com")
-    addFriend(db, "newvava@gmail.com","leon@zwamy.com")
-
-    addFriend(db, "zhangruiddn@gmail.com","newvava@gmail.com")
-    addFriend(db, "newvava@gmail.com","zhangruiddn@gmail.com")
-    #deleteUser(db, "zhangruiddn@gmail.com")
-
+    test(db,bucket)
     conn.close()
 
 if __name__ == "__main__":
